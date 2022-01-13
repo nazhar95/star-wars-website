878778b6f05e17b3f320fcfb52593ada352fd92e
Naz Azhar <nazhar.telebort@gmail.com>
2022-01-13 12:19:00 +0000

    commit3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,28 @@
 <script>
+    let id="";
     let name="";
-    let planet="";
+    let gender="";
+    let homeworld="";
     let height="";
+    let weight="";
+    let hair_color="";
+    let eye_color="";
+    let birth_year="";
+    let description="";
     
-
-    async function getWeather() {
-        fetch('https://goweather.herokuapp.com/weather/' + location)
+    async function getInfo() {
+        fetch('https://www.swapi.tech/api/people/' + id)
             .then((response) => response.json())
             .then((data) => {
-                weatherTemperature = data.temperature; // Path for temperature
-                weatherDescription = data.description; // Path for description
+                name = data.name; 
+                gender = data.gender;
+                homeworld = data.homeworld; 
+                height = data.height; 
+                weight = data.mass; 
+                hair_color = data.hair_color; 
+                eye_color = data.eye_color;
+                birth_year = data.birth_year;
+                description = data.description;  
             });
     }
 
@@ -20,15 +33,27 @@
 </div>
 
 <div class="container text-center mt-3">
-    <h2>Type in a character's name</h2>
+    <h2>Type in a character's ID</h2>
 
-    <form on:submit|preventDefault={getWeather}>
-        <label for="location">Location:</label>
-        <input id="location" bind:value={location} type="text" />
+    <form on:submit|preventDefault={getInfo}>
+        <label for="ID">Character ID:</label>
+        <input id="id" bind:value={id} type="text" />
         <button type="submit">Check</button>
     </form>
     
-    <p>Temperature: {weatherTemperature}</p>
-    <p>Description: {weatherDescription}</p>
-</div>
+    <div class="card text-white bg-primary">
+        <div class="card-header">Name: {name}</div>
+            <div class="card-body">
+                <h5 class="card-title">Info:</h5>
+                <p class="card-text">Gender: {gender}</p>
+                <p class="card-text">Homeworld: {homeworld}</p>
+                <p class="card-text">Height: {height}cm</p>
+                <p class="card-text">Weight: {weight}kg</p>
+                <p class="card-text">Hair color: {hair_color}</p>
+                <p class="card-text">Eye color: {eye_color}</p>
+                <p class="card-text">Birth year: {birth_year}</p>
+                <p class="card-text">Description: {description}</p>
+            </div>
+        </div>
+    </div>
 
