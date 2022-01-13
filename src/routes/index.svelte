0522dd80992e91b092e109ef0fc1c13b237d5eab
Naz Azhar <nazhar.telebort@gmail.com>
2022-01-13 12:42:32 +0000

    commit4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,33 @@
 <script>
-    let id="";
+    let uid="";
     let name="";
     let gender="";
-    let homeworld="";
     let height="";
     let weight="";
     let hair_color="";
     let eye_color="";
     let birth_year="";
-    let description="";
     
     async function getInfo() {
-        fetch('https://www.swapi.tech/api/people/' + id)
+        fetch('https://www.swapi.tech/api/people/' + uid)
             .then((response) => response.json())
             .then((data) => {
-                name = data.name; 
-                gender = data.gender;
-                homeworld = data.homeworld; 
-                height = data.height; 
-                weight = data.mass; 
-                hair_color = data.hair_color; 
-                eye_color = data.eye_color;
-                birth_year = data.birth_year;
-                description = data.description;  
+                name = data.result.properties.name; 
+                gender = data.result.properties.gender;
+                height = data.result.properties.height; 
+                weight = data.result.properties.mass; 
+                hair_color = data.result.properties.hair_color; 
+                eye_color = data.result.properties.eye_color;
+                birth_year = data.result.properties.birth_year;
             });
     }
-
 </script>
+
+<style>
+    .card-text {
+    text-transform: capitalize;
+    }
+</style>
 
 <div class="container-fluid bg-dark text-light p-5 text-center">
    <h1>Star Wars 101</h1>
@@ -37,22 +38,19 @@
 
     <form on:submit|preventDefault={getInfo}>
         <label for="ID">Character ID:</label>
-        <input id="id" bind:value={id} type="text" />
+        <input id="id" bind:value={uid} type="number" />
         <button type="submit">Check</button>
     </form>
     
-    <div class="card text-white bg-primary">
-        <div class="card-header">Name: {name}</div>
-            <div class="card-body">
-                <h5 class="card-title">Info:</h5>
+    <div class="card bg-primary mt-3 mb-3 w-50 m-auto">
+        <div class="card-header bg-dark text-light">Name: {name}</div>
+            <div class="card-body bg-light">
                 <p class="card-text">Gender: {gender}</p>
-                <p class="card-text">Homeworld: {homeworld}</p>
                 <p class="card-text">Height: {height}cm</p>
                 <p class="card-text">Weight: {weight}kg</p>
                 <p class="card-text">Hair color: {hair_color}</p>
                 <p class="card-text">Eye color: {eye_color}</p>
                 <p class="card-text">Birth year: {birth_year}</p>
-                <p class="card-text">Description: {description}</p>
             </div>
         </div>
     </div>
